bb8be79f45a20b6d781374890314165be68fa48e
Priyabrat <ppadhy@regalix-inc.com>
2016-03-11 17:03:09 +0530

    ewfvfv

---
+++ practices/scripts/form_validation.js
@@ -0,0 +1,5 @@
+var app = angular
+			.module("appValidate"[])
+			.controller("controllerValidate"function(){
+				
+			});
---
+++ routingexample/scripts/script.js
@@ -0,0 +1,28 @@
+var app = angular
+			.module("demoApp",["ngRoute"])
+			.config(function($routeProvider) {
+				$routeProvider
+					.when("/home", {
+						templateUrl: "templetes/home.html",
+						controller: "homeController"
+					})
+					.when("/courses", {
+						templateUrl: "templetes/cources.html",
+						controller: "courcesController"
+					})
+					.when("/students", {
+						templateUrl: "templetes/students.html",
+						controller: "studentsController"
+					})
+					.otherwise({
+						redirectTo:"/home"
+					});
+			})
+			.controller("homeController", function($scope) {
+				$scope.message = "Home Page (By Priyabrat)";
+			})
+			.controller("courcesController", function($scope) {
+				$scope.courses = ["Java","C","C++","Android"];
+			}).controller("studentsController", function($scope) {
+				$scope.students = ["Ram","Lakshman","Shyam","John"];
+			});
